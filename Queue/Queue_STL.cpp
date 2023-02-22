11713d1f46987588c87a2d5c175ce9867cba4936
Yash Sahane <yashsahane23@gmail.com>
2023-02-22 19:09:25 +0530

    done

--- Queue/Queue_STL.cpp
+++ Queue/Queue_STL.cpp
@@ -9,22 +9,27 @@ main()
     q.push(10);
     cout << "front is " << q.front() << endl;
     cout << "back is " << q.back() << endl;
+    cout << "size is " << q.size() << endl;
 
     q.push(20);
     cout << "front is " << q.front() << endl;
     cout << "back is " << q.back() << endl;
+    cout << "size is " << q.size() << endl;
 
     q.push(30);
     cout << "front is " << q.front() << endl;
     cout << "back is " << q.back() << endl;
+    cout << "size is " << q.size() << endl;
 
     q.pop();
     cout << "front is " << q.front() << endl;
     cout << "back is " << q.back() << endl;
+    cout << "size is " << q.size() << endl;
 
     q.pop();
     cout << "front is " << q.front() << endl;
     cout << "back is" << q.back() << endl;
+    cout << "size is " << q.size() << endl;
 
     if (q.empty())
     {
