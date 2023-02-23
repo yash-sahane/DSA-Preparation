5850f3fab3b537ea8bf1d250a81bc5dc5560c66a
Yash Sahane <yashsahane23@gmail.com>
2023-02-23 21:16:34 +0530

    done

---
+++ Queue/Queue_reversal_recursion.cpp
@@ -0,0 +1,19 @@
+void solve(queue<int> &q)
+{
+    if (q.empty())
+    {
+        return;
+    }
+
+    int element = q.front();
+    q.pop();
+
+    solve(q);
+    q.push(element);
+}
+
+queue<int> rev(queue<int> q)
+{
+    solve(q);
+    return q;
+}
