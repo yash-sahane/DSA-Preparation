cd743939944d1a5e7cef8a897664664356e687a2
Yash Sahane <yashsahane23@gmail.com>
2023-02-27 21:38:59 +0530

    done

---
+++ Binary Tree/implementation_of_binary_tree.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *buildTree(Node *root)
+{
+    cout << "Enter the data" << endl;
+    int data;
+    cin >> data;
+
+    root = new Node(data);
+
+    if (data == -1)
+    {
+        return NULL;
+    }
+
+    cout << "Enter the left data for " << data << " : ";
+    root->left = buildTree(root->left);
+
+    cout << "Enter the right data for " << data << " : ";
+    root->right = buildTree(root->right);
+
+    return root;
+}
+
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> queue;
+    queue.push(root);
+
+    while (!queue.empty())
+    {
+        Node *temp = queue.front();
+        cout << temp->data << " ";
+        queue.pop();
+
+        if (temp->left)
+        {
+            queue.push(temp->left);
+        }
+        if (temp->right)
+        {
+            queue.push(temp->right);
+        }
+    }
+}
+
+main()
+{
+    Node *root = NULL;
+
+    root = buildTree(root);
+
+    levelOrderTraversal(root);
+}
