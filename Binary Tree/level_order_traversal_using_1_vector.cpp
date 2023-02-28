6265904c3bafc5d826c847496d4f5a14b83379f6
Yash Sahane <yashsahane23@gmail.com>
2023-02-28 20:01:36 +0530

    done

---
+++ Binary Tree/level_order_traversal_using_1_vector.cpp
@@ -0,0 +1,36 @@
+// GFG
+
+class Solution
+{
+public:
+    // Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node *node)
+    {
+        vector<int> result;
+
+        if (node == NULL)
+        {
+            return result;
+        }
+
+        queue<Node *> q;
+        q.push(node);
+
+        while (!q.empty())
+        {
+            Node *temp = q.front();
+            q.pop();
+            result.push_back(temp->data);
+
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+        return result;
+    }
+};
