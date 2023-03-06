d49d1fb2e24be18066e6b83befc4a6579ec1e764
Yash Sahane <yashsahane23@gmail.com>
2023-03-06 20:57:53 +0530

    Lecture 64

---
+++ Binary Tree/bottom_view_of_binary_tree.cpp
@@ -0,0 +1,40 @@
+vector<int> bottomView(Node *root)
+{
+    vector<int> result;
+    if (root == NULL)
+    {
+        return result;
+    }
+
+    queue<pair<Node *, int>> q;
+    map<int, int> topNode;
+
+    q.push(make_pair(root, 0));
+
+    while (!q.empty())
+    {
+        pair<Node *, int> temp = q.front();
+        Node *front = temp.first;
+        q.pop();
+
+        int hd = temp.second;
+        topNode[hd] = front->data;
+
+        if (front->left)
+        {
+            q.push(make_pair(front->left, hd - 1));
+        }
+
+        if (front->right)
+        {
+            q.push(make_pair(front->right, hd + 1));
+        }
+    }
+
+    for (auto i : topNode)
+    {
+        result.push_back(i.second);
+    }
+
+    return result;
+}
